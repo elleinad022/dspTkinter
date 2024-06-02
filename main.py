95a2401df2873f8d689e581c4c4a499d43e8985e
elleinad <janjaaan12@gmail.com>
2024-06-03 00:28:18 +0800

    Added windowFour and some labels

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import tkinter.filedialog as fd
+
+windowFour = tk.Tk()
+windowFour.geometry("800x500")
+windowFour.title("HP/LP switchable")
+
+label1 = tk.Label(windowFour, text="High pass and Low pass switchable filter", font=('Arial 20 underline'))
+label1.pack(pady=20)
+
+label2 = tk.Label(windowFour, text="Select a wav file to filter : ", font=('Arial 14'))
+label2.pack()
+
+windowFour.configure()
+
+
+windowFour.mainloop()
