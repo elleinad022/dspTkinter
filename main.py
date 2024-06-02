eb1f7fcea137850b48191a952b14d1a576f65fbe
elleinad <janjaaan12@gmail.com>
2024-06-03 00:57:08 +0800

    Added open file button and its function that takes file path

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import tkinter.filedialog as fd
 
+def openFile():
+    filePath=fd.askopenfilename(title="OPEN A WAV FILE. OKAY?", filetypes=(("Audio wav files","*.wav"), ))
+    print(filePath)
+
 windowFour = tk.Tk()
 windowFour.geometry("800x500")
 windowFour.title("HP/LP switchable")
@@ -10,6 +14,9 @@ label1.pack(pady=20)
 
 label2 = tk.Label(windowFour, text="Select a wav file to filter : ", font=('Arial 14'))
 label2.pack()
+
+selectBtn = tk.Button(text="Open File", command=openFile)
+selectBtn.pack()
 
 
 
