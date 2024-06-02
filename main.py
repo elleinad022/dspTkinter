4acc44dc607a0c59df195f3c9ed691481b99f952
elleinad <janjaaan12@gmail.com>
2024-06-03 01:05:51 +0800

    Made window not resizable and added button padding

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def openFile():
     print(filePath)
 
 windowFour = tk.Tk()
-windowFour.geometry("800x500")
+windowFour.resizable(0,0)
+windowFour.geometry("700x350")
 windowFour.title("HP/LP switchable")
 
 label1 = tk.Label(windowFour, text="High pass and Low pass switchable filter", font=('Arial 20 underline'))
@@ -15,8 +16,8 @@ label1.pack(pady=20)
 label2 = tk.Label(windowFour, text="Select a wav file to filter : ", font=('Arial 14'))
 label2.pack()
 
-selectBtn = tk.Button(text="Open File", command=openFile)
-selectBtn.pack()
+selectBtn = tk.Button(text="Open File", command=openFile, padx=30 , pady=30)
+selectBtn.pack(pady=20)
 
 
 
