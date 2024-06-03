96fa15d761e7fdfde6f29e701e1f9d2e23f9678e
elleinad <janjaaan12@gmail.com>
2024-06-03 14:54:48 +0800

    Updated filterHP to produce filter plot

--- testFiltersHP.py
+++ testFiltersHP.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import signal
 import librosa
 import soundfile as sf
+import matplotlib.pyplot as plt
 
 def butter_highpass(cutoff, fs, order=5):
     nyq = 0.5 * fs
@@ -14,6 +15,20 @@ def butter_highpass_filter(data, cutoff, fs, order=5):
     y = signal.filtfilt(b, a, data)
     return y
 
+def plot_filter_response(cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    w, h = signal.freqz(b, a, worN=8000)
+    plt.figure(figsize=(12, 6))
+    plt.plot(0.5 * fs * w / np.pi, np.abs(h), 'b')
+    plt.plot(cutoff, 0.5 * np.sqrt(2), 'ko')
+    plt.axvline(cutoff, color='k', linestyle='--')
+    plt.xlim(0, 0.5 * fs)
+    plt.title("High-Pass Filter Frequency Response")
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Gain')
+    plt.grid()
+    plt.show()
+
 # Load audio file using librosa
 audio, fs = librosa.load('input_audio_hp.wav', sr=None, mono=False)
 
@@ -21,20 +36,20 @@ audio, fs = librosa.load('input_audio_hp.wav', sr=None, mono=False)
 cutoff_frequency = 1000  # 1000 Hz cutoff frequency
 order = 5  # Order of the filter
 
-# If the audio is stereo, separate the channels
+# Apply high-pass filter
 if audio.ndim == 2:
+    # Handle stereo audio
     audio_left = audio[0, :]
     audio_right = audio[1, :]
-    
-    # Apply high-pass filter to both channels
     filtered_audio_left = butter_highpass_filter(audio_left, cutoff_frequency, fs, order)
     filtered_audio_right = butter_highpass_filter(audio_right, cutoff_frequency, fs, order)
-    
-    # Combine the filtered channels
     filtered_audio = np.vstack((filtered_audio_left, filtered_audio_right))
 else:
-    # If the audio is mono, just filter it
+    # Handle mono audio
     filtered_audio = butter_highpass_filter(audio, cutoff_frequency, fs, order)
 
-# Save filtered audio to a new file using soundfile
+# Save filtered audio to a new file
 sf.write('filtered_audio_hp.wav', filtered_audio.T, fs)
+
+# Plot the frequency response of the filter
+plot_filter_response(cutoff_frequency, fs, order)
